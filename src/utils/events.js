35c68076ba1bc4ab0db547f5b1d069b1354f22b0
Drew Bolles <drewbolles@gmail.com>
2018-03-24 12:26:08 -0400

    Add event utils

---
+++ src/utils/events.js
@@ -0,0 +1,28 @@
+export const events = [
+  { name: 'NBA Basketball', sport: 'basketball' },
+  { name: 'NCAA Basketball', sport: 'basketball' },
+  { name: 'NFL Football', sport: 'football' },
+  { name: 'NCAA Football', sport: 'football' },
+  { name: 'MLB Baseball', sport: 'baseball' },
+  { name: 'NCAA Baseball', sport: 'baseball' },
+  { name: 'Golf', sport: 'golf' },
+];
+
+export const stockBets = [
+  {
+    sport: 'basketball',
+    bets: [
+      { bet: 'Makes both free throws' },
+      { bet: 'Defensive stop' },
+      { bet: 'Offensive Rebound' },
+    ],
+  },
+  {
+    sport: 'golf',
+    bets: [
+      { bet: 'Closest to the pin' },
+      { bet: 'Sandy' },
+      { bet: 'Longest Drive' },
+    ],
+  },
+];
